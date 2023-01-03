da24f501c7f081252b291d0f51e616f1043359ad
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-01-04 00:07:39 +0100

    Added Services

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoinDataService } from '../coin-data.service';
 
 @Component({
   selector: 'app-coins',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./coins.component.css']
 })
 export class CoinsComponent {
+
+ constructor(private coinService: CoinDataService){}
 
 }
